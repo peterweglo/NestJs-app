580c988937bdcfb6ec7ef554520293e1bde0a4a7
Piotr Węgłowski <weglowskipiotr@gmail.com>
2024-04-21 14:05:34 +0200

    Add endpoints

---
+++ src/products/products.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Param, Delete } from '@nestjs/common';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
+  @Get('/')
+  getAll(): any {
+    return this.productsService.getAll();
+  }
+
+  @Get('/:id')
+  public getById(@Param('id') id: string) {
+    return this.productsService.getById(id);
+  }
+
+  @Delete('/:id')
+  deleteById(@Param('id') id: string) {
+    this.productsService.deleteById(id);
+    return { success: true };
+  }
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { db, Product } from './../db';
+
+@Injectable()
+export class ProductsService {
+  public getAll(): Product[] {
+    return db.products;
+  }
+
+  public getById(id: Product['id']): Product | null {
+    return db.products.find((p) => p.id === id);
+  }
+
+  public deleteById(id: Product['id']): void {
+    db.products = db.products.filter((p) => p.id !== id);
+  }
+}
