fe366aad4852146d28c683c5f6231105369394ea
Piotr Węgłowski <weglowskipiotr@gmail.com>
2024-05-13 11:04:39 +0200

    Update products and orders endpoints with prisma

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,5 +1,17 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Delete,
+  Post,
+  Body,
+  Put,
+  NotFoundException,
+} from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { ParseUUIDPipe } from '@nestjs/common';
+import { CreateOrderDTO } from './dtos/create-order-dto';
+import { UpdateOrderDTO } from './dtos/update-order-dto';
 
 @Controller('orders')
 export class OrdersController {
@@ -11,7 +23,34 @@ export class OrdersController {
   }
 
   @Get('/:id')
-  getById(@Param('id') id: string) {
-    return this.ordersService.getById(id);
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const ord = await this.ordersService.getById(id);
+    if (!ord) throw new NotFoundException('Order not found');
+    return ord;
+  }
+
+  @Delete('/:id')
+  async deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.ordersService.getById(id)))
+      throw new NotFoundException('Order not found');
+    await this.ordersService.deleteById(id);
+    return { success: true };
+  }
+
+  @Post('/')
+  create(@Body() orderData: CreateOrderDTO) {
+    return this.ordersService.create(orderData);
+  }
+
+  @Put('/:id')
+  async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() orderData: UpdateOrderDTO,
+  ) {
+    if (!(await this.ordersService.getById(id)))
+      throw new NotFoundException('Order not found');
+
+    await this.ordersService.updateById(id, orderData);
+    return { success: true };
   }
 }
--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
+import { PrismaService } from '../shared/services/prisma.service';
 
 @Module({
-  providers: [OrdersService],
   controllers: [OrdersController],
+  providers: [OrdersService, PrismaService],
 })
 export class OrdersModule {}
